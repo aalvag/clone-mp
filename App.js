2f73c1869d3a1b95455e9f6f98450eab6906e7a0
David Alvarez <aalvag@live.com>
2022-04-10 01:55:54 -0300

    add react

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 import Navigation from "./src/navigation";
